15e87a54bc16d78390bad98efa8c28cc82c5e7fb
nianjiuhuiyi <nianjiuhuiyi@vip.qq.com>
2024-05-10 16:17:29 +0800

    第一张图再往前，最后一张图再往后有弹窗提示

--- salt/interface.py
+++ salt/interface.py
@@ -5,6 +5,9 @@ from PyQt5.QtGui import QImage, QPixmap, QPainter, QWheelEvent, QMouseEvent, QCl
 from PyQt5.QtCore import Qt, QRectF
 from PyQt5.QtWidgets import QPushButton, QRadioButton, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QMessageBox, QInputDialog
 
+from .editor import Editor
+
+
 class CustomGraphicsView(QGraphicsView):
     def __init__(self, editor):
         super(CustomGraphicsView, self).__init__()
@@ -50,11 +53,12 @@ class CustomGraphicsView(QGraphicsView):
         new_pos = self.mapToScene(event.pos())
         delta = new_pos - old_pos
         self.translate(delta.x(), delta.y())
-    
+
     def imshow(self, img):
         height, width, channel = img.shape
         bytes_per_line = 3 * width
-        q_img = QImage(img.data, width, height, bytes_per_line, QImage.Format_RGB888).rgbSwapped()
+        q_img = QImage(img.data, width, height, bytes_per_line,
+                       QImage.Format_RGB888).rgbSwapped()
         self.set_image(q_img)
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
@@ -64,12 +68,13 @@ class CustomGraphicsView(QGraphicsView):
         if event.button() == Qt.LeftButton:
             label = 1
         elif event.button() == Qt.RightButton:
-            label = 0        
+            label = 0
         self.editor.add_click([int(x), int(y)], label)
         self.imshow(self.editor.display)
-    
+
+
 class ApplicationInterface(QWidget):
-    def __init__(self, app, editor, panel_size=(1920, 1080)):
+    def __init__(self, app, editor: Editor, panel_size=(1920, 1080)):
         super(ApplicationInterface, self).__init__()
 
         self.app = app
@@ -81,9 +86,8 @@ class ApplicationInterface(QWidget):
         self.top_bar = self.get_top_bar()
         self.layout.addWidget(self.top_bar)
 
-        
         self.main_window = QHBoxLayout()
-        
+
         self.graphics_view = CustomGraphicsView(self.editor)
         self.main_window.addWidget(self.graphics_view)
 
@@ -94,34 +98,45 @@ class ApplicationInterface(QWidget):
         self.setLayout(self.layout)
 
         self.graphics_view.imshow(self.editor.display)
-    
+
     def reset(self):
         self.editor.reset()
-        self.graphics_view.imshow(self.editor.display)    
+        self.graphics_view.imshow(self.editor.display)
 
     def add(self):
         self.editor.save_ann()
         self.editor.reset()
-        self.graphics_view.imshow(self.editor.display)    
+        self.graphics_view.imshow(self.editor.display)
 
     def delet(self):
         self.editor.delet_ann()
         self.editor.reset()
-        self.graphics_view.imshow(self.editor.display)   
-        
+        self.graphics_view.imshow(self.editor.display)
+
     def next_image(self):
+        if self.editor.image_id + 1 == self.editor.imgs_num:
+            QMessageBox.warning(self, "警告", "这已经是最后一张图了！")
+            return
+
         self.editor.next_image()
         self.graphics_view.imshow(self.editor.display)
         self.editor.save()
 
     def prev_image(self):
+        # 注意这种实例化弹窗方式与上面直接调用静态方法，静态方法更简便，
+        # 但下面实例化对象后，可以其它时候再执行，且.exec_()会有返回值，代表用户点的那个按钮。
+        if self.editor.image_id == 0:
+            msg_box = QMessageBox(QMessageBox.Warning, "警告", "已经是第一张图了！")
+            msg_box.exec_()
+            return
+
         self.editor.prev_image()
-        self.graphics_view.imshow(self.editor.display)    
+        self.graphics_view.imshow(self.editor.display)
 
     def toggle(self):
         self.editor.toggle()
-        self.graphics_view.imshow(self.editor.display)    
-    
+        self.graphics_view.imshow(self.editor.display)
+
     def process(self):
         # 为了显示一下进度
         info = "当前进度：{}/{}".format(self.editor.image_id + 1, self.editor.imgs_num)
@@ -130,7 +145,8 @@ class ApplicationInterface(QWidget):
 
     def jump(self):
         # 为了图片跳转
-        value, success = QInputDialog.getInt(self, "图片跳转", f"请输入[1,{self.editor.imgs_num}]中的整数: ", value=self.editor.image_id + 1, min=1, max=self.editor.imgs_num, step=1)
+        value, success = QInputDialog.getInt(
+            self, "图片跳转", f"请输入[1,{self.editor.imgs_num}]中的整数: ", value=self.editor.image_id + 1, min=1, max=self.editor.imgs_num, step=1)
         if success:
             self.editor.image_id = value - 1 - 1  # 为了显示，给的值都比索引大1，所以减去1
             self.editor.next_image()   # 然后这里面对self.editor.image_id加了1，所以上面还要再减去1，
@@ -143,7 +159,7 @@ class ApplicationInterface(QWidget):
     def transparency_down(self):
         self.editor.step_down_transparency()
         self.graphics_view.imshow(self.editor.display)
-    
+
     def save_all(self):
         self.editor.save()
 
@@ -162,7 +178,7 @@ class ApplicationInterface(QWidget):
             ("跳转", lambda: self.jump()),
             ("调高透明度", lambda: self.transparency_up()),
             ("调低透明度", lambda: self.transparency_down()),
-            ("保存", lambda: self.save_all()), 
+            ("保存", lambda: self.save_all()),
         ]
         for button, lmb in buttons:
             bt = QPushButton(button)
@@ -182,19 +198,21 @@ class ApplicationInterface(QWidget):
 
             label = QRadioButton(category)
             # sender传入点击的字符
-            label.toggled.connect(lambda: self.editor.select_category(self.sender().text()))
+            label.toggled.connect(
+                lambda: self.editor.select_category(self.sender().text()))
             panel_layout.addWidget(label)
         return panel
 
     # 这个函数是继承于父类的，这里我重写了,采用异步保存数据，必须等线程池先写完，不然直接退出json文件就坏了。
-    # 这是点击右上角的x关闭按钮触发的时间
+    # 这是点击右上角的x关闭按钮触发的事件
     def closeEvent(self, event: QCloseEvent) -> None:
-        reply = QMessageBox.question(self, u'警告', u'确认退出?', QMessageBox.Yes, QMessageBox.No)
-        if reply==QMessageBox.Yes:
+        reply = QMessageBox.question(
+            self, u'警告', u'确认退出?', QMessageBox.Yes, QMessageBox.No)
+        if reply == QMessageBox.Yes:
             event.accept()  # 关闭窗口
             # 点击确定后，再保存一下
             self.save_all()
-            
+
             # 之后得留时间让线程池的保存执行完，不然会损坏json文件
             self.editor.pool.close()
             self.editor.pool.join()
@@ -204,7 +222,7 @@ class ApplicationInterface(QWidget):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            
+
             # 退出前再保存一下
             self.save_all()
             # 在关闭软件前先关闭线程池，并通过join阻塞祖先成，文件保存完后才退出。
